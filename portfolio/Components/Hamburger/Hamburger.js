07abba42e1d958200ca0250e20b95681992dcf2e
NavroO <42099711+NavroO@users.noreply.github.com>
2020-06-13 19:09:10 +0200

    Added files for Hamburger menu

---
+++ portfolio/Components/Hamburger/Hamburger.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Hamburger.css';
+
+export default class Hamburger extends React.Component {
+    render() {
+        return <img className="HamburgerBar" src="./HamburgerIcon.png" alt=""></img>;
+    }
+}
