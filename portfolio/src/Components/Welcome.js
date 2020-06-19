33bfe6cec432dbf9c88bafb838508df44011c5b4
NavroO <42099711+NavroO@users.noreply.github.com>
2020-06-19 13:47:57 +0200

    Better project files structure

--- portfolio/src/Components/Welcome.js
+++ portfolio/src/Components/Welcome.js
@@ -6,11 +6,20 @@ class Welcome extends Component {
         return (
             <div className="WelcomeDiv">
                 <div className="bg"></div>
-                <p className="Text" id="hey">Hey There!</p>
-                <p className="Text" id="welcome">I’m <p className="name">Tomasz Nawrocki</p></p>
-                <p className="Text" id="city">Based In Katowice</p>
-                <div className="Label"></div>
-                <div className="Label2"></div>
+
+                <div className="Welcome_Text">
+                    <div className="Red_Label"></div>
+                    <p className="Text" id="hey">Hey There!</p>
+                    <p className="Text" id="welcome">I’m <p className="name">Tomasz Nawrocki</p></p>
+                    <p className="Text" id="city">Based In Katowice</p>
+                </div>
+
+                <div className="Skills">
+                    <div className="Label"></div>
+                    <div className="Label2"></div>
+                </div>
+
+                
                 <p className="labelText" id="wd">Web Designer</p>
                 <p className="labelText" id="wb">Web Developer</p>
                 <p className="labelText" id="ui">UI/UX Designer</p>
