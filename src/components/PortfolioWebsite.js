57328aaaf736f7ec2f15aee61f5414ca3949fac2
Navro <tomeknawrocki2000@gmail.com>
2021-02-16 10:59:25 +0100

    Fixed bug with header

--- src/components/PortfolioWebsite.js
+++ src/components/PortfolioWebsite.js
@@ -77,7 +77,7 @@ const StyledTextSpecialization = styled(StyledText)`
 `
 
 const StyledSocialContainer = styled.div`
-    display: inline-block;
+    display: inline;
 `;
 
 const StyledParagraph = styled.p`
