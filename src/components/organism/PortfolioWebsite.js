6cafa8f46e97fd17bc316496e825c5482076ef95
Navro <tomeknawrocki2000@gmail.com>
2021-03-30 17:02:01 +0200

    Added animation

--- src/components/organism/PortfolioWebsite.js
+++ src/components/organism/PortfolioWebsite.js
@@ -1,5 +1,7 @@
 /* eslint-disable react/style-prop-object */
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Footer from '../molecules/Footer/Footer';
 import Contact from '../molecules/Contact/Contact';
 import MainSkillSecection from '../molecules/MainSkillsSection/MainSkillSection';
@@ -9,14 +11,32 @@ import Hero from '../molecules/Hero/Hero';
 
 const PortfolioWebiste = () => {
 
+    const wrapper = useRef(null);
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        const sections = document.querySelectorAll('section');
+
+
+        sections.forEach(section => {
+            gsap.fromTo(section.children, { y: '+=100', opacity: 0 }, {
+                y: 0, opacity: 1, stagger: 0.2, duration: 1, ease: 'easeInOut', scrollTrigger: {
+                    trigger: section,
+                    start: 'top 20%',
+                }
+            });
+        });
+    });
+
     return (
-        <>
+        <div ref={wrapper}>
             <Hero />
             <MainSkillSecection />
             <PortfolioSection />
             <Contact />
             <Footer />
-        </>
+        </div>
     );
 }
 
